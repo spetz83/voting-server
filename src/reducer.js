52e3e963d50cae747c6f9bebd6ae13e994881f02
Tom Etzel <tom.etzel@quantum.com>
2017-07-09 22:54:03 -0500

    make vote function only care about the vote state, not entire app state

--- src/reducer.js
+++ src/reducer.js
@@ -4,13 +4,10 @@ export default function reducer(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'SET_ENTRIES':
             return setEntries(state, action.entries);
-            break;
         case 'NEXT':
             return next(state);
-            break;
         case 'VOTE':
-            return vote(state, action.entry);
-            break;
+            return state.update('vote', voteState => vote(voteState, action.entry));
     }
     return state;
 }
